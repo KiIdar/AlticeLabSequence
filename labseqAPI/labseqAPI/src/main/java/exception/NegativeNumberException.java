508d6b958d2ab8e6a645013c66f6fbd0c29d14b1
Kildar <74959404+KiIdar@users.noreply.github.com>
2024-01-29 17:35:41 +0000

    Delete labseqAPI directory

--- labseqAPI/labseqAPI/src/main/java/exception/NegativeNumberException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Value must be a non-negative integer number.")
-public class NegativeNumberException extends RuntimeException{
-	
-}
